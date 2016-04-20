dd79476d0c0c3eb2b324baeb8dd82ec691eb9bcb
Marion Le Hérisson <marion.hurteau@le118000.fr>
2016-04-20 14:19:30 +0200

    const.php : config différentes en fonction de l'environnement (Wamp/Mamp)

--- const.php
+++ const.php
@@ -2,8 +2,13 @@
 /* C O N F   P R O D */
 //define('BASE_URL', 'http://coligo.fr.nf/');
 
+//echo realpath ( __FILE__ ).'----'.__FILE__ ;
+
+//print_r($_SERVER);
+
 /* C O N F   L O C A L */ 
-define('BASE_URL', 'http://localhost:8888/ProjAnnuel2016/');
+//define('BASE_URL', 'http://localhost:8888/ProjAnnuel2016/');
+define('BASE_URL', 'http://www.coligo.local.lan/');
 
 define('HOSTNAME', 'localhost');
 define('DBNAME', 'ColiGo');
