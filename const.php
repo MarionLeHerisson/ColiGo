cfd63d187305fce692f0e841cc6d6978d2c4190a
MarionLeHeisson <marion.hurteau1@gmail.com>
2016-04-22 09:55:02 +0200

    conf Mac

--- const.php
+++ const.php
@@ -1,5 +1,5 @@
 <?php
-
+//print_r($_SERVER);die;
 /* C O N F   L O C A L   W I N D O W S   M A R I O N */
 if($_SERVER['HTTP_HOST'] == 'www.coligo.local.lan') {
 
@@ -12,9 +12,9 @@ if($_SERVER['HTTP_HOST'] == 'www.coligo.local.lan') {
     define('DBPWD', 'root');
 }
 /* C O N F   L O C A L   M A C */
-else if($_SERVER['HTTP_HOST'] == 'http://localhost:8888/') {
+else if($_SERVER['HTTP_HOST'] == 'localhost:8888') {
 
-    define('BASE_URL', 'http://localhost:8888/ProjAnnuel2016/');
+    define('BASE_URL', 'localhost:8888/ProjAnnuel2016/');
     define('BASE_PATH', '/Applications/MAMP/htdocs/ProjAnnuel2016/');
 
     define('HOSTNAME', 'localhost');
